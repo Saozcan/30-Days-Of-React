e36f590cf06f1c8b623d8d7814aab7d934790573
saozcan <samimozcan@outlook.com>
2022-08-17 16:46:07 +0300

    Day 23

--- 23_Fetching_Data_Using_Hooks/23_fetching_data_using_hooks_boilerplate/src/index.js
+++ 23_Fetching_Data_Using_Hooks/23_fetching_data_using_hooks_boilerplate/src/index.js
@@ -1,57 +1,48 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect} from 'react'
 import axios from 'axios'
 import ReactDOM from 'react-dom'
 
-const Country = ({ country: { name, flag } }) => {
-  return (
-    <div className='country'>
-      <div className='country_flag'>
-        <img src={flag} alt={name} />
-      </div>
-      <h3 className='country_name'>{name.toUpperCase()}</h3>
-      <div class='country_text'>
-        <p>
-          <span>Population: </span>
-        </p>
-      </div>
-    </div>
-  )
-}
+
+
+
+
+
+
+
+
+
 
 const App = (props) => {
-  // setting initial state and method to update state
-  const [data, setData] = useState([])
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+	const [counter, setCounter] = useState(0)
 
-  const fetchData = async () => {
-    const url = 'https://restcountries.eu/rest/v2/all'
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-      setData(data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+useEffect(() => {
+	getData();
+}, [])
 
-  return (
-    <div className='App'>
-      <h1>Fetching Data Using Hook</h1>
-      <h1>Calling API</h1>
-      <div>
-        <p>There are {data.length} countries in the api</p>
-        <div className='countries-wrapper'>
-          {data.map((country) => (
-            <Country country={country} />
-          ))}
-        </div>
-      </div>
-    </div>
-  )
+const getData = async () => {
+	const url = 'https://api.thecatapi.com/v1/breeds';
+	try {
+		const response = await axios.get(url);
+		const data = await response.data;
+		console.log(data);
+	}
+	catch (error) {
+		console.log(error);
+	}
+}
+
+const handleClick = () => {
+	setCounter(counter + 1);
+}
+
+	return (
+		<div className='App'>
+			<button onClick={handleClick}>Hello</button>
+			<span>{counter}</span>
+		</div>
+	)
 }
 
 const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(<App />, rootElement);
